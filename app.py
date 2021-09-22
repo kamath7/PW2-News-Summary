900d6fd1b1c93e46ebe11b5e70151f9248b1e830
kamath7 <adithyakamath96@gmail.com>
2021-09-23 02:41:15 +0530

    initial summarizing

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print('hello. I will summarize news')
+import tkinter as tk 
+import nltk 
+from newspaper import Article
+
+nltk.download('punkt')
+url = "https://timesofindia.indiatimes.com/india/will-focus-on-covid-terror-climate-crisis-at-un-pm-modi/articleshow/86439798.cms"
+article = Article(url)
+article.download()
+article.parse()
+
+article.nlp()
+print('Title: {0}'.format(article.title,))
+print('Authors: {0}'.format(article.authors,))
+print('Published Date: {0}'.format(article.publish_date,))
+print('Summary: {0}'.format(article.summary,))
